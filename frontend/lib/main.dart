198c36a340580c179c265ad30a3baf12f2a0b9dd
Leen Abdel Rhman <leenabusalem1@gmail.com>
2026-04-22 14:54:40 +0300

    enabled remember me option

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,47 @@
-
 import 'package:flutter/material.dart';
-import 'package:frontend/welcomepage.dart';
-import 'package:provider/provider.dart'; 
-import 'providers/favorites_provider.dart'; 
-import 'welcomepage.dart'; 
-import'home_page.dart';
 
-void main() {
+import 'package:frontend/home_page.dart';
+import 'package:provider/provider.dart';
+import 'providers/favorites_provider.dart';
+import 'aiAssistant_page.dart';
+import'personal_info_page.dart';
+import 'welcomepage.dart';
+import 'main_wrapper.dart';
+import 'explorPage.dart';
+import 'hotelBoookingPage.dart';
+import 'events_page.dart';
+import 'admin_page.dart';
+import 'profile.dart';
+import "home_page.dart";
+import 'auth_page.dart';
+import "auth_service.dart";
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final storage = const FlutterSecureStorage();
+  bool remember = await shouldRememberMe();
+  String? accessToken = await storage.read(key: 'access_token');
+
+  if (!remember) {
+    await logout();
+  }
+
+  Widget initialPage = (remember && accessToken != null) ?  HomePage() :  AuthPage();
+
   runApp(
-
-    MultiProvider(
+   MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => FavoritesProvider()),
       ],
-      child: const MyApp(),
+      child: MaterialApp(
+        home: initialPage,
+        navigatorKey: navigatorKey,
+      ),
     ),
   );
 }
